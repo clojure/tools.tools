ea3df106dc7dcac7806ca3376735294ce95b9972
Alex Miller <alex.miller@cognitect.com>
2021-04-28 15:29:04 -0500

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+tools.tools
+========================================
+
+A set of functions for managing Clojure CLI tools.
+
+Also see:
+
+* [Getting Started](https://clojure.org/guides/getting_started)
+* [Deps and CLI Guide](https://clojure.org/guides/deps_and_cli)
+* [Deps and CLI Reference](https://clojure.org/reference/deps_and_cli)
+* [Tool implementation and installers](https://github.com/clojure/brew-install)
+
+# Release Information
+
+Latest release: none yet
+
+[deps.edn](https://clojure.org/guides/deps_and_cli) dependency information:
+
+```
+io.github.cognitect-labs/tools.tools {:git/tag "vT.B.D" :git/sha "TBDTBD"}
+```
+
+# API 
+
+
+# Developer Information
+
+* [GitHub project](https://github.com/clojure/tools.tools)
+* [How to contribute](https://clojure.org/community/contributing)
+* [Bug Tracker](https://clojure.atlassian.net/browse/TDEPS)
+
+# Copyright and License
+
+Copyright © 2021 Rich Hickey, Alex Miller, and contributors
+
+All rights reserved. The use and
+distribution terms for this software are covered by the
+[Eclipse Public License 1.0] which can be found in the file
+LICENSE at the root of this distribution. By using this software
+in any fashion, you are agreeing to be bound by the terms of this
+license. You must not remove this notice, or any other, from this
+software.
+
+[Eclipse Public License 1.0]: https://opensource.org/licenses/eclipse-1.0.php
---
+++ src/clojure/tools/tools/api.clj
@@ -0,0 +1,120 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns clojure.tools.tools.api
+  "This api provides functions that can be executed from the Clojure tools using -Ttools."
+  (:refer-clojure :exclude [list remove])
+  (:require
+    [clojure.java.io :as jio]
+    [clojure.pprint :as pprint]
+    [clojure.string :as str]
+    [clojure.tools.deps.alpha :as deps]
+    [clojure.tools.deps.alpha.tool :as tool]
+    [clojure.tools.deps.alpha.tree :as tree]
+    [clojure.tools.deps.alpha.script.resolve-tags :as resolve-tags]
+    [clojure.tools.deps.alpha.extensions :as ext]
+    [clojure.tools.deps.alpha.extensions.pom :as pom]
+    [clojure.tools.deps.alpha.extensions.local :as local]
+    [clojure.tools.deps.alpha.extensions.git :as git]
+    [clojure.tools.deps.alpha.gen.pom :as gen-pom]
+    [clojure.tools.deps.alpha.util.maven :as mvn]
+    [clojure.tools.deps.alpha.util.io :as io :refer [printerrln]]
+    [clojure.tools.gitlibs :as gitlibs])
+  (:import
+    [java.io File FileNotFoundException IOException]
+    [java.nio.file Files]
+    [java.nio.file.attribute FileAttribute]
+    [java.util.jar JarFile]
+    [org.apache.maven.model Model]
+    [org.eclipse.aether.artifact DefaultArtifact]
+    [org.eclipse.aether.installation InstallRequest]
+    [clojure.lang IExceptionInfo]))
+
+(set! *warn-on-reflection* true)
+
+(defn install
+  "Install a tool for later use, taking the following required fields:
+    lib - lib name, and coord value
+    :as - tool name
+  On install, the tool is procured, and persisted with the tool name for later use."
+  [{:keys [as] :as args}]
+  (let [lib (first (filter qualified-symbol? (keys args)))
+        coord (get args lib)]
+    (when (or (not lib) (not coord) (not as))
+      (println "Missing required args: lib to coord or :as"))
+    (let [{:keys [root-edn user-edn]} (deps/find-edn-maps)
+          master-edn (deps/merge-edns [root-edn user-edn])
+          coord (let [{:git/keys [url sha tag]} coord
+                      url (if (nil? url) (git/auto-git-url lib) url)]
+                  (cond-> coord
+                    (and tag (nil? sha))
+                    (assoc :git/sha (gitlibs/resolve url tag))))
+          [lib coord] (ext/canonicalize lib coord master-edn)]
+      (when-not (and lib coord)
+        (throw (ex-info (format "Could not resolve tool: %s" (pr-str args)) args)))
+      (tool/install-tool lib coord as)
+      (println "Installed" as))))
+
+(defn- max-len
+  [vals]
+  (apply max (map #(count (str %)) vals)))
+
+(defn- pad
+  [s len]
+  (format (str "%-" len "s") s))
+
+(defn- print-table
+  [ks rows]
+  (let [widths (map (fn [k] (+ 2 (max-len (map k rows)))) (butlast ks))]
+    (run!
+      (fn [row]
+        (let [padded-cols (map #(pad (get row %1) %2) ks widths)]
+          (println (str (apply str padded-cols) (get row (last ks))))))
+      rows)))
+
+(defn list
+  "List available tools"
+  [_]
+  (print-table [:tool :lib :type :version]
+    (cons
+      {:tool "TOOL" :lib "LIB" :type "TYPE" :version "VERSION"}
+      (map (fn [tool]
+             (let [{:keys [lib coord]} (tool/resolve-tool tool)
+                   ctype (ext/coord-type coord)
+                   {mver :mvn/version gtag :git/tag} coord]
+               {:tool tool :lib lib :type ctype :version (or mver gtag)}))
+        (tool/list-tools)))))
+
+(defn show
+  "Print info and usage for this :tool"
+  [{:keys [tool] :as args}]
+  (let [{:keys [lib coord] :as info} (tool/resolve-tool tool)]
+    (if info
+      (do
+        (println "Info for" tool ":")
+        (println)
+        (println "lib:" lib)
+        (println "coord:")
+        (binding [*print-namespace-maps* false]
+          (pprint/pprint coord)))
+      (println "Tool not found"))))
+
+(defn remove
+  "Remove :tool, if it exists."
+  [{:keys [tool] :as args}]
+  (if tool
+    (if (tool/remove-tool tool)
+      (println "Tool removed")
+      (println "Tool not found or could not be removed"))))
+
+(comment
+  (install '{io.github.seancorfield/clj-new
+                  {:git/tag "v1.1.243"} :as "clj-new"})
+
+  (list nil)
+  )
